39bf75a2154d13f9bef53bf53a5cb11bdd3dc436
Tori FavWest <practiceandthenpractice@gmail.com>
2021-05-13 18:07:14 -0700

    change fragment layout for patient1

--- app/src/main/java/com/favwest/medicalrpg/Patient1Fragment.kt
+++ app/src/main/java/com/favwest/medicalrpg/Patient1Fragment.kt
@@ -1,19 +1,33 @@
 package com.favwest.medicalrpg
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.text.Layout
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.RenderProcessGoneDetail
+import androidx.databinding.DataBindingUtil
+import com.favwest.medicalrpg.databinding.ActivityMainBinding.inflate
+import com.favwest.medicalrpg.databinding.FragmentPatient1Binding
 
 class Patient1Fragment : Fragment() {
-
+    lateinit var binding: FragmentPatient1Binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_patient1, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_patient1, container, false)
+        binding.apply {
+            binding.painManagement.setOnClickListener {showPainManagementOptions()}
+        }
+        return binding.root
+    }
+
+    fun showPainManagementOptions(): Unit {
+        binding.painManagementOptions.visibility = View.GONE
     }
 
 }
--- app/src/main/java/com/favwest/medicalrpg/SelectPatientFragment.kt
+++ app/src/main/java/com/favwest/medicalrpg/SelectPatientFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.favwest.medicalrpg.databinding.FragmentSelectPatientBinding
 
 class SelectPatientFragment : Fragment() {
@@ -15,6 +16,9 @@ class SelectPatientFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding: FragmentSelectPatientBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_select_patient, container, false)
+        binding.selectPatient1Button.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_selectPatientFragment_to_patient1Fragment)
+        )
         return binding.root
     }
 }
