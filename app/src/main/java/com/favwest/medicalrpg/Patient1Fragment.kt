27090fd4a47e0876536cff2dd333943938dad427
Tori FavWest <practiceandthenpractice@gmail.com>
2021-05-14 09:37:16 -0700

    button clicks for Patient1 change button appearance correctly

--- app/src/main/java/com/favwest/medicalrpg/Patient1Fragment.kt
+++ app/src/main/java/com/favwest/medicalrpg/Patient1Fragment.kt
@@ -1,19 +1,17 @@
 package com.favwest.medicalrpg
 
-import android.opengl.Visibility
 import android.os.Bundle
-import android.text.Layout
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.RenderProcessGoneDetail
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
-import com.favwest.medicalrpg.databinding.ActivityMainBinding.inflate
+import androidx.fragment.app.Fragment
 import com.favwest.medicalrpg.databinding.FragmentPatient1Binding
 
 class Patient1Fragment : Fragment() {
     lateinit var binding: FragmentPatient1Binding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,13 +19,28 @@ class Patient1Fragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_patient1, container, false)
         binding.apply {
-            binding.painManagement.setOnClickListener {showPainManagementOptions()}
+            painManagement.setOnClickListener {showPainManagementOptions()}
+            completePainManagement.setOnClickListener {hidePainManagementOptions()}
+            val painMngButtons = listOf(morphine, dilaudid)
+            for (button in painMngButtons) {
+                button.setOnClickListener{changeButtonSelected(button)}
+            }
         }
         return binding.root
     }
 
-    fun showPainManagementOptions(): Unit {
-        binding.painManagementOptions.visibility = View.GONE
+    private fun showPainManagementOptions() {
+        if (binding.painManagementOptions.visibility == View.GONE)
+            binding.painManagementOptions.visibility = View.VISIBLE
     }
 
+    private fun hidePainManagementOptions() {
+        if (binding.painManagementOptions.visibility == View.VISIBLE)
+            binding.painManagementOptions.visibility = View.GONE
+    }
+
+    private fun changeButtonSelected(button: Button) {
+        button.isSelected = !button.isSelected
+        if (button.isSelected) button.setBackgroundColor(resources.getColor(R.color.blue)) else button.setBackgroundColor(resources.getColor(R.color.red))
+    }
 }
